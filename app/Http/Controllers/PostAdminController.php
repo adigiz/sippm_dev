432307a7ed7056514f72f67621389fef2fd3af4d
Gizwanda <adigiz@yahoo.com>
2020-01-26 16:34:05 +0700

    [Giz] fix and add warning for admin post functionality

--- app/Http/Controllers/PostAdminController.php
+++ app/Http/Controllers/PostAdminController.php
@@ -19,18 +19,19 @@ class PostAdminController extends Controller
     {
         $this->middleware('auth:admin');
     }
+
     public function index()
     {
-//        $data['posts'] = Post::orderBy('created_at','desc')->get();
-//        $id_post = Post::orderBy('created_at','desc')->get()->pluck('file_id');
-//        $data['files'] = File::whereIn('id',$id_post)->get();
-        $data['posts'] = DB::table('posts')
-            ->leftJoin('files', 'posts.file_id', '=', 'files.id')
-            ->select('posts.id','posts.judul', 'posts.isi', 'posts.created_at','nama_file')
-            ->orderBy('posts.created_at','desc')
-            ->paginate(2);
-
-        return view('admin/post.index',$data);
+        if(Post::all() -> isNotEmpty()) {
+            $data = DB::table('posts')
+                ->leftjoin('files', 'posts.id', '=', 'files.post_id')
+                ->select('posts.id','posts.judul', 'posts.isi', 'posts.created_at','nama_file')
+                ->orderBy('posts.created_at','desc')
+                ->paginate(2);
+            return view('admin/post.index', compact('data'));
+        }
+        $data = NULL;
+        return view('admin/post.index', compact('data'))->with('alert-warning','Belum ada pengumuman yang dibuat');
     }
 
     /**
@@ -51,35 +52,24 @@ class PostAdminController extends Controller
      */
     public function store(Request $request)
     {
-        $file = $request->file('attach');
-        $files = new File();
-        $files->nama_file = time().'-'.$file->getClientOriginalName();
-        $files->ukuran_file =  $file->getClientSize();
-        $files->ekstensi_file = $file->getClientOriginalExtension();
-        $nm = time().'-'.$file->getClientOriginalName();
-        $file->move('uploads/file',$nm);
-        $files->save();
-
-        $id_file = $files->id;
-
         $data = new Post();
         $data->judul = $request->judul;
         $data->isi = $request->isi;
         $data->users_id = Auth::guard('admin')->id();
-        $data->file_id = $id_file;
         $data->save();
-        return redirect()->route('post.index')->with('alert-success','Berhasil Menambahkan Data!');
-    }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Post  $post
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Post $post)
-    {
-        //
+        $file = $request->file('attach');
+        $files = new File();
+        if($file != null ){
+            $files->nama_file = time().'-'.$file->getClientOriginalName();
+            $files->ukuran_file =  $file->getClientSize();
+            $files->ekstensi_file = $file->getClientOriginalExtension();
+            $files-> post_id = $data ->id;
+            $nm = time().'-'.$file->getClientOriginalName();
+            $file->move('uploads/file',$nm);
+            $files->save();
+        }
+        return redirect()->route('post.index')->with('alert-success','Berhasil Menambahkan Data!');
     }
 
     /**
@@ -120,9 +110,8 @@ class PostAdminController extends Controller
     public function destroy($id)
     {
         $data = Post::where('id',$id)->first();
-        $id_file = $data->file_id;
-        if(File::where('id',$id_file)->exists()){
-            $file = File::where('id',$id_file)->first();
+        if(File::where('post_id',$id)->exists()){
+            $file = File::where('post_id',$id)->first();
             $file_path = 'uploads/file/'.$file->nama_file;
             unlink($file_path);
             $file->delete();
--- resources/views/admin/post/index.blade.php
+++ resources/views/admin/post/index.blade.php
@@ -7,40 +7,48 @@
         <div class="alert alert-success">
             <strong>{{ \Illuminate\Support\Facades\Session::get('alert-success') }}</strong>
         </div>
+    @elseif(\Illuminate\Support\Facades\Session::has('alert-warning'))
+            <div class="alert alert-warning">
+                <strong>{{ \Illuminate\Support\Facades\Session::get('alert-warning') }}</strong>
+            </div>
     @endif
     <div class="row">
-        <div class="col-12">
-            @foreach($posts as $berita)
-                <div class="card">
-                    <div class="card-block">
-                        <h4 class="card-title">{{$berita->judul}}
-                            <br>
-                            <small class="text-muted">{{date('d-m-Y',strtotime($berita->created_at))}}</small>
-                        </h4>
-
-                        <hr>
-                        <p class="card-text">
-                            {{$berita->isi}}
-                            <br>
-                            @if($berita->nama_file != NULL)
-                                <a href="{{asset("uploads/file/")."/".$berita->nama_file}}">{{$berita->nama_file}}</a>
-                            @endif
-
-                        </p>
-                    </div>
-                    <form action="{{ route('post.destroy', $berita->id) }}" method="post">
+        @if($data !== NULL)
+            <div class="col-12">
+                @foreach($data as $berita)
+                    <div class="card">
                         <div class="card-block">
-                            {{ csrf_field() }}
-                            {{ method_field('DELETE') }}
-                            <a href="{{ route('post.edit',$berita->id) }}" class=" btn btn-warning">Edit</a>
-                            <button class="btn btn-danger" type="submit" onclick="return confirm('Yakin ingin menghapus data?')">Delete</button>
+                            <h4 class="card-title">{{$berita->judul}}
+                                <br>
+                                <small class="text-muted">{{date('d-m-Y',strtotime($berita->created_at))}}</small>
+                            </h4>
+
+                            <hr>
+                            <p class="card-text">
+                                {{$berita->isi}}
+                                <br>
+                                @if($berita->nama_file != NULL)
+                                    <a href="{{asset("uploads/file/")."/".$berita->nama_file}}">{{$berita->nama_file}}</a>
+                                @endif
+
+                            </p>
                         </div>
-                    </form>
+                        <form action="{{ route('post.destroy', $berita->id) }}" method="post">
+                            <div class="card-block">
+                                {{ csrf_field() }}
+                                {{ method_field('DELETE') }}
+                                <a href="{{ route('post.edit',$berita->id) }}" class=" btn btn-warning">Edit</a>
+                                <button class="btn btn-danger" type="submit"
+                                        onclick="return confirm('Yakin ingin menghapus data?')">Delete
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                @endforeach
+                <div class="text-center">
+                    {{$data->links()}}
                 </div>
-            @endforeach
-            <div class="text-center">
-                {{$posts->links()}}
             </div>
-        </div>
+        @endif
     </div>
 @endsection
